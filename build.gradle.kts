63016eb54e8b902129db991faa276e8c95e001e4
Bryan Sills <bryan@bryansills.com>
2019-02-05 23:03:11 -0600

    add dagger dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,21 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.11"
+    id("org.jetbrains.kotlin.kapt") version "1.3.20"
 }
 
 group = "bryansills.ninja"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
 }
 
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.google.dagger:dagger:2.21")
+    kapt("com.google.dagger:dagger-compiler:2.21")
 }
 
 tasks.withType<KotlinCompile> {
